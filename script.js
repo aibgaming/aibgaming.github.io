cf029e4b61e4ca3d117111f43a4cc04eddd4ef05
Autri <autri12@gmail.com>
2022-07-24 14:30:59 +0530

    arroy funcs

--- script.js
+++ script.js
@@ -1,40 +1,87 @@
-// // $(document).on("scroll", function() {
-// //     var pageTop = $(document).scrollTop();
-// //     var pageBottom = pageTop + $(window).height();
-// //     var tags = $(".tag");
+// $(document).on("scroll", function() {
+//     var pageTop = $(document).scrollTop();
+//     var pageBottom = pageTop + $(window).height();
+//     var tags = $(".tag");
   
-// //     for (var i = 0; i < tags.length; i++) {
-// //       var tag = tags[i];
+//     for (var i = 0; i < tags.length; i++) {
+//       var tag = tags[i];
   
-// //       if ($(tag).position().top < pageBottom) {
-// //         $(tag).addClass("visible");
-// //       } else {
-// //         $(tag).removeClass("visible");
-// //       }
-// //     }
-// //   });
+//       if ($(tag).position().top < pageBottom) {
+//         $(tag).addClass("visible");
+//       } else {
+//         $(tag).removeClass("visible");
+//       }
+//     }
+//   });
 
-// const timer = document.querySelector('#time span')
+const timer = document.querySelector('#time span')
 
-// setInterval(() => {
-// 	timer.innerText = new Date().toLocaleString()
-// }, 1000)
+setInterval(() => {
+	timer.innerText = new Date().toLocaleString()
+}, 1000)
 
-$(window).on("load",function() {
-	$(window).scroll(function() {
-	  var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-	  $(".faded").each(function() {
-		/* Check the location of each desired element */
-		var objectBottom = $(this).offset().top + $(this).outerHeight();
-		
-		/* If the element is completely within bounds of the window, fade it in */
-		if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-		  if ($(this).css("opacity")==0.25) {$(this).fadeTo(500,1);}
-		} else { //object goes out of view (scrolling up)
-		  if ($(this).css("opacity")==1) {$(this).fadeTo(500,0.25);}
+// ---------------------------------------------------------------
+
+
+const ulElement = document.getElementById("list-items") //list item
+const counterEL = document.getElementById("counter")   // counter obj
+
+let counter = 0
+function incrementCounter(){
+	counter++
+	counterEL.innerText = counter
+
+	const li = document.createElement('li')
+	li.setAttribute('data-counter',counter)
+
+	if (counter%2 == 0){
+		li.style.background = 'red'
+	} else {
+		li.style.background = 'yellow'
+		li.style.padding = '20px'
+	}
+
+	li.innerHTML = '<b>Sentence </b>' + counter
+	
+	ulElement.appendChild(li) 
+}
+
+function decrementCounter(){
+
+	if (counter > 0 ){
+		const li = ulElement.querySelector('[data-counter="' + counter + '"]')
+		const number = parseInt(li.getAttribute('data-counter'), 10)
+
+		if(number%2==0){
+			li.remove()
+			
 		}
-	  });
-	}).scroll(); //invoke scroll-handler on page-load
-  });
+		counter--
+		
+		
+	}
+	counterEL.innerText = counter
+}
 
-  
+const incrementBtn = document.querySelector("#increment")
+const decrementBtn = document.querySelector("#decrement")
+
+incrementBtn.addEventListener('click',incrementCounter)
+decrementBtn.addEventListener('click',decrementCounter)
+
+
+// ES6 / ES2015 
+
+const arr = [1,2,3,4,5,6,7,8]
+
+for (let i = 0; i < arr.length;i++){
+	const el = arr[i]
+
+}
+
+const newMappedArray = arr.map(function(element){
+	console.log(element)
+	return 100
+})
+
+console.log(arr, newMappedArray)
